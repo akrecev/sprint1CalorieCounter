755f069c8e96ab42cee305c9e13718d1d15b7566
akrecev <akrecev@gmail.com>
2022-07-02 23:15:08 +0300

    correction

--- src/com/akrecev/caloriecounter/Converter.java
+++ src/com/akrecev/caloriecounter/Converter.java
@@ -5,16 +5,11 @@ public class Converter {
     final int FOR_DISTANCE = 750;
     final int FOR_KILOCALORIES = 20;
 
-    double convertDistance;
-    double convertKCalories;
-
     double convertToKilometer(int steps) {
-        convertDistance = steps / FOR_DISTANCE;
-        return convertDistance;
+        return steps / FOR_DISTANCE;
     }
 
     double convertToKCalories(int steps) {
-        convertKCalories = steps / FOR_KILOCALORIES;
-        return convertKCalories;
+        return steps / FOR_KILOCALORIES;
     }
 }
